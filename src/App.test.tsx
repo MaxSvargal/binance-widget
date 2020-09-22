2388a24c5ffb36a9aff6fc43bc188101444e59ad
MaxSvargal <maxsvargal@gmail.com>
2020-09-22 19:54:28 +0700

    Enable App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,12 @@
+import fetch from 'jest-fetch-mock';
 import React from 'react';
+
 import { render } from '@testing-library/react';
+
 import App from './App';
 
 test('App renders correctly', () => {
+  fetch.mockResponseOnce(() => Promise.resolve(JSON.stringify({})));
   const { getByText } = render(<App />);
   const perfectCoin = getByText(/BNB/i);
   expect(perfectCoin).toBeInTheDocument();
