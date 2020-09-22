add815c7e5354247d7c14b80519d0c60a5519790
MaxSvargal <maxsvargal@gmail.com>
2020-09-22 23:29:02 +0700

    Remove unused useSelectParentMarkets hook

--- src/tickerWidget/hooks/useSelectParentMarkets.ts
+++
@@ -1,7 +0,0 @@
-import { useMemo } from 'react';
-import { IProduct } from '../interfaces/products';
-
-export const useSelectParentMarkets = (products: Array<IProduct>): string[] =>
-  useMemo(() => [...new Set(products.map((product) => product.pm))], [
-    products,
-  ]);
